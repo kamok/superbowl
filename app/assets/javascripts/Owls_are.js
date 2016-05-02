d45e3e09d0847be441e870dfb60ca3f2f55400a4
Ka Mok <heykamok@gmail.com>
2016-05-02 16:56:51 -0400

    Add font family, favicon, and javascript for frontpage owl attributes.

---
+++ app/assets/javascripts/Owls_are.js
@@ -0,0 +1,13 @@
+$(function() {
+	var divs = $('div[id^="content-"]').hide(),
+	    i = 0;
+
+	$(function cycle() { 
+
+	    divs.eq(i).fadeIn(400)
+	              .delay(1200)
+	              .fadeOut(400, cycle);
+
+	    i = ++i % divs.length;
+	})
+});

6321b9fa6a592b80271b796130bf65b01eaee287
Ka Mok <heykamok@gmail.com>
2016-05-26 19:56:58 -0400

    Remove json format from posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,38 +20,36 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.build(post_params)
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.save
+       flash[:notice] = "Post has been created."
+       redirect_to @post
+    else
+      flash[:alert] = "Post has not been created."
+      render "new"
     end
   end
 
   def update
-    respond_to do |format|
+
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
+        flash[:notice] = "Post has been updated."
+        redirect_to @post
       else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        flash[:notice] = "Post has not been updated."
+        render "edit"
       end
-    end
   end
 
   def destroy
     @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully deleted.' }
-      format.json { head :no_content }
-    end
+
+    flash[:notice] = "Post has been deleted."
+
+    redirect_to posts_url
   end
 
   private
+  
     def set_post
       @post = Post.find_by(id: params[:id])
     end

11e184db5d4347052f5456628203796447b60467
Ka Mok <heykamok@gmail.com>
2016-05-26 18:14:27 -0400

    Add controller to handle callback url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
  Rails.application.routes.draw do
   resources :posts
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   get 'about' => 'pages#about'
   root 'posts#index'
 

fe257d85c6c0d0cb20b86b668c8d3f929cb6ba1a
Ka_Lenovo <heykamok@gmail.com>
2016-06-04 23:47:41 -0400

    Add model specs

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ group :test do
   gem 'factory_girl_rails', '~> 4.2.1'
   gem 'shoulda-matchers', '~> 3.1'
   gem 'simplecov', require: false
+  gem 'shoulda-matchers', '~> 3.1'
 end
 
 group :development do
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,24 +3,7 @@ require 'rails_helper'
 describe Post do
   let(:post) { Post.create(title: "Foo", body: "Bar") }
 
-  it { should validate_presence_of (:title) }
-
-  describe "#pretty_updated_at" do
-    it "makes the updated_at date look Month/Day/Year format" do
-      time = Time.local(2016, 4, 5)
-      Timecop.freeze(time)
-
-      expect(post.pretty_updated_at).to eq("4/5/2016")
-    end
-  end
-
-  describe "#pretty_created_at" do
-    it "makes the created_at date look Month/Day/Year format" do
-      time = Time.local(2017, 5, 6)
-      Timecop.freeze(time)
-
-      expect(post.pretty_created_at).to eq("5/6/2017")
-    end
-  end
+  it { should validate_presence_of (:image) }
+  it { should validate_presence_of (:description) }
   
 end 
